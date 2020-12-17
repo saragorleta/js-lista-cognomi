955273572d4f438314643650d0d2e4145af7aabb
Sara <saragorleta@yahoo.com>
2020-12-17 15:14:49 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente (occhio alle maiuscole, se volete potete convertire il nome prima inserirlo).
+// Scrivi anche la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova
+
+// chiediamo il cognome all'utente
+var cognome=prompt('inserisci il tuo cognome');
+// lista cognomi
+var listaCognomi=[‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’];
+// aggiunta del cognome
+listaCognomi.push(Gorleta);
+
+console.log(listaCognomi);
+
+
+
+//per metterli in ordine alfabetico
+// var myArr = Array.from("ABCDEFG");
+// var listaCognomi=Array.from(‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’);
+
+// per ordinarli da 1 a 6
+// fruits.sort();
+// listaCognomi.sort('');
